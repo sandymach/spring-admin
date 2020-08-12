76b488541d42018bde44b0289b30aef0e68329b5
sandyamachani <sandyamachani@gmail.com>
2020-08-12 19:25:08 +0530

    spring-admin

---
+++ src/main/java/com/cts/pss/SpringAdminApplication.java
@@ -0,0 +1,44 @@
+package com.cts.pss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@Configuration
+@SpringBootApplication
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+
+	@Configuration
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+            .formLogin()
+            .loginPage("/login")
+            .permitAll();
+        http
+            .logout().logoutUrl("/logout").permitAll();
+        http.csrf()
+            .ignoringAntMatchers("/actuator/**", "/instances/**", "/logout");
+        http
+            .authorizeRequests()
+            .antMatchers("/**/*.css", "/assets/**", "/third-party/**", "/logout", "/login")
+            .permitAll();
+        http
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated();
+        http.httpBasic(); // Activate Http basic Auth for the server
+    }
+		
+	}
+}
